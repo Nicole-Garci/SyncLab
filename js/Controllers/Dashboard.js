eb32753783eb41d34fe74ca331519d117f3e90e3
VictorBorgesLoures <victorblp@hotmail.com>
2024-09-19 20:01:50 -0300

    fix sidebar

--- js/Controllers/Dashboard.js
+++ js/Controllers/Dashboard.js
@@ -39,6 +39,34 @@ const data = {
 
 window.onload = () => {
 
+    let sidebarIsOpen = true;
+
+    document.getElementById("sidebar-btn").onclick = (event => {
+        event.preventDefault();
+        if (!sidebarIsOpen) {
+            document.getElementById("side-navbar").className = "side-bar active";
+            document.getElementById("main-content").className = "main-content active";
+            let logoElements = document.getElementsByClassName("navlogo");
+            let length = logoElements.length;
+            for (let i = 0; i < length; i++) {
+                let element = logoElements[i];
+                let classes = element.className.split(" ");
+                element.className = "";
+                for (let j = 0; j < classes.length - 1; j++) {
+                    element.className += " " + classes[j];
+                }
+            }
+        } else {
+            document.getElementById("side-navbar").className = "side-bar";
+            document.getElementById("main-content").className = "main-content";
+            let logoElements = document.getElementsByClassName("navlogo");
+            for (let i = 0; i < logoElements.length; i++) {
+                logoElements[i].className += " active";
+            }
+        }
+        sidebarIsOpen = !sidebarIsOpen;
+    });
+
     const stackedLine = new Chart(ctx, {
         type: 'line',
         data: data,
